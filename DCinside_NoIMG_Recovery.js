e4dad25d5f69309e34281d3107449bd749540cec
TetraTheta <tetratheta@gmail.com>
2019-01-13 09:28:33 +0000

    업데이트 README.md, DCinside_NoIMG_Recovery.js 파일

---
+++ DCinside_NoIMG_Recovery.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         DCinside NoIMG Recovery
+// @namespace    TetraTheta
+// @version      0.1-190113
+// @description  Replacing NoIMG error and force-loading original image
+// @author       TetraTheta
+// @include      *.dcinside.co.kr/*
+// @include      *.dcinside.com/*
+// @run-at       document-start
+// ==/UserScript==
+
+document.onreadystatechange = function() {
+    if (document.readyState === "interactive") {
+        var data = document.getElementsByTagName("img");
+        for(var i=0;i<data.length;i++){
+            if (data[i].src.indexOf("http://dcimg") != -1 && data[i].src.indexOf(".dcinside.co.kr/viewimage.php?") != -1) {
+                data[i].src="http://image.dcinside.com/viewimage.php?"+data[i].src.split("?")[1];
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# TetraUserJS
-
-Some Userscript made by TetraTheta.
+# TetraUserJS  
+Some UserScript made by TetraTheta. Description are below.  
+---
+* `DCinside_NoIMG_Recovery.js`  
+This script is for DCinside. Recently DCinside got DDOS attack, so many images doesn't load properly.  
+This script replace 'No Image' error(엑스박스) to original image.  
