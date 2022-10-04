ee57aad8d0a31c5c1a80bb896da2e52e3358b942
TetraTheta <tetratheta@gmail.com>
2022-10-05 02:37:12 +0900

    GitHub Word Wrap

---
+++ scripts/GitHub/GitHub_WordWrap.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         GitHub Word Wrap
+// @namespace    https://github.com/TetraTheta
+// @version      0.1.0.0T-221005
+// @description  Apply word wrap in GitHub code pages
+// @author       TetraTheta
+// @match        *://github.com/*
+// @match        *://gist.github.com/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle(`
+  .blob-code-inner, .markdown-body pre > code, .markdown-body .highlight > pre {
+    white-space: pre-wrap !important;
+    word-break: break-all !important;
+    overflow-wrap: break-word !important;
+    display: block !important;
+  }
+  td.blob-code-inner {
+    display: table-cell !important;
+  }
+`);
---
+++ scripts/GitHub/README.md
@@ -0,0 +1,3 @@
+**GitHub_WordWrap.user.js**
+=========================
+This script will word wrap GitHub's code pages.
