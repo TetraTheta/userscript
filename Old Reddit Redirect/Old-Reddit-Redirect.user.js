0e8d437f17ff0d448f3a5d5d54abab11f6b64e67
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2024-02-14 17:55:48 +0900

    Add 'Old Reddit Redirect'

---
+++ Old Reddit Redirect/Old-Reddit-Redirect.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name Old Reddit Redirect
+// @namespace tetratheta
+// @version 1.0.0
+// @description Conditional old reddit redirector
+// @author TetraTheta
+// @match https://*.reddit.com/*
+// @icon https://reddit.com/favicon.ico
+// @grant none
+// @run-at document-start
+// @updateURL https://github.com/TetraTheta/TetraUserScripts/raw/main/Old%20Reddit%20Redirect/Old-Reddit-Redirect.user.js
+// @downloadURL https://github.com/TetraTheta/TetraUserScripts/raw/main/Old%20Reddit%20Redirect/Old-Reddit-Redirect.user.js
+// ==/UserScript==
+
+(function() {
+  'use strict'
+  function hasNewQueryString(url) { return url.includes('new=true') }
+  function redirectToOldReddit() {
+    if (window.location.hostname !== 'old.reddit.com' && !hasNewQueryString(window.location.href)) {
+      window.location.replace('https://old.reddit.com' + window.location.pathname + window.location.search + window.location.hash)
+    }
+  }
+  function redirectToNewReddit() {
+    if (window.location.hostname === 'old.reddit.com' && hasNewQueryString(window.location.href)) {
+      window.location.replace('https://www.reddit.com' + window.location.pathname + window.location.search + window.location.hash);
+    }
+  }
+  if (window.location.hostname.includes('reddit.com')) {
+    if (window.location.hostname === 'www.reddit.com' && !window.location.hostname.includes('old.reddit.com')) {
+      redirectToOldReddit();
+    } else if (window.location.hostname === 'old.reddit.com') {
+      redirectToNewReddit();
+    }
+  }
+})();
---
+++ Old Reddit Redirect/README.md
@@ -0,0 +1,6 @@
+# Old Reddit Redirect
+[Install](https://github.com/TetraTheta/TetraUserScripts/raw/main/Old%20Reddit%20Redirect/Old-Reddit-Redirect.user.js)
+
+I prefer `old.reddit.com` than `www.reddit.com`. But sometimes, I have to use `www.reddit.com`.
+
+By default, you'll be redirected to `old.reddit.com`. To prevent this, append `new=true` query string to URL like `reddit.com/?new=true`. This will prevent redirection to `old.reddit.com`.
