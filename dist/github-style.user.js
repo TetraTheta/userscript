aee0496fb8e9a338b98f138811aa3a7a20dd4a8e
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-04-15 13:25:28 +0900

    feat: Add 'github-style.user.js'

---
+++ dist/github-style.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name GitHub Style
+// @namespace tetratheta
+// @version 1.0.0
+// @description Customized GitHub
+// @author TetraTheta
+// @grant none
+// @match *://*.github.com/*
+// @run-at document-start
+// @noframes
+// @updateURL https://tetratheta.github.io/userscript/github-style.user.js
+// @downloadURL https://tetratheta.github.io/userscript/github-style.user.js
+// ==/UserScript==
+
+'use strict'
+
+function GM_addStyle(aCss) {
+  let head = document.getElementsByTagName('head')[0]
+  if (head) {
+    let style = document.createElement('style')
+    style.setAttribute('type', 'text/css')
+    style.textContent = aCss
+    head.appendChild(style)
+    return style
+  }
+  return null
+}
+
+(() => {
+  const regular_font = "'Pretendard', 'Spoqa Han Sans Neo', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Nanum Gothic', Arial, sans-serif !important;"
+  const monospace_font = "'goorm Sans Code', 'D2Coding', 'Cascadia Code PL', ui-monospace, SFMono-Regular, 'SF Mono', Menlo, Consolas, 'Liberation Mono', monospace !important;"
+
+  const addStyle = css => {
+    const style = document.createElement('style');
+    style.textContent = css;
+    document.head.appendChild(style);
+  }
+
+  const css_general = `
+    :root {
+      --fontStack-monospace: ${monospace_font};
+      --fontStack-sansSerif: ${regular_font};
+      --fontStack-sansSerifDisplay: ${regular_font};
+      --fontStack-system: ${regular_font};
+
+      font-family: ${regular_font};
+    }
+    .Fragment, .blob-code, .blob-code-content, .blob-code-inner, .markdown-body code, .markdown-body pre, .react-code-text, .text-mono, code, kbd, pre, samp, span[role='presentation'], textarea {
+      font-family: ${monospace_font};
+    }
+  `
+
+  addStyle(css_general)
+
+  const onReady = () => {
+    document.body.style.cssText += font;
+  }
+
+  const observer = new MutationObserver(applyReservationStyle)
+  observer.observe(document.documentElement, { childList: true, subtree: true })
+  if (document.readyState === 'loading') document.addEventListener('DOMContentLoaded', onReady)
+  else onReady()
+})();
